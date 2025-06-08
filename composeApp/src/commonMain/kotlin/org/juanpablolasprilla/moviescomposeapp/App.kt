a2c9501d739f99b568a49ec601cfd6098baef6ef
Juan Pablo Lasprilla Correa <juanpablolasprillacorrea@gmail.com>
2025-06-08 10:26:54 -0500

    The attributes of LazyVerticalGrid were selected

--- composeApp/src/commonMain/kotlin/org/juanpablolasprilla/moviescomposeapp/App.kt
+++ composeApp/src/commonMain/kotlin/org/juanpablolasprilla/moviescomposeapp/App.kt
@@ -2,16 +2,22 @@ package org.juanpablolasprilla.moviescomposeapp
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -22,6 +28,15 @@ import moviescomposeapp.composeapp.generated.resources.compose_multiplatform
 @Preview
 fun App() {
     MaterialTheme {
-        Text("Hello world!")
+        Surface(modifier = Modifier.fillMaxSize()) {
+            LazyVerticalGrid(
+                columns = GridCells.Adaptive(120.dp),
+                contentPadding = PaddingValues(4.dp),
+                horizontalArrangement = Arrangement.spacedBy(4.dp),
+                verticalArrangement = Arrangement.spacedBy(4.dp)
+            ){
+
+            }
+        }
     }
 }
